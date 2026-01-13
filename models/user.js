31f93f6211c4fa34d725c794b3a3a4d2d52556c7
Banitha Madushan <m20000904012@student.cinec.edu>
2026-01-13 13:57:59 +0530

    Create whatsapp attribute in user model.

--- models/user.js
+++ models/user.js
@@ -30,7 +30,12 @@ const userSchema = new mongoose.Schema({
     phone : {
         type: String,
         required: true
+    },
+    whatsApp : {
+        type: String,
+        required: true
     }
+
 });
 
 const User = mongoose.model("User",userSchema);
